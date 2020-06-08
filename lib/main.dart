ebcf4b6ccf70aeba526b8a64065e0ba627413c9d
Adeyoriju Olabode Wilson <whilson03@gmail.com>
2020-06-08 01:18:50 +0100

    explore containers in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -6,82 +6,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-      ),
-      home: HomePage(
-        title: "Explore Flutter",
-      ),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  final String title;
-
-  HomePage({Key key, this.title}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          FloatingActionButton(
-            tooltip: "Increment Counter",
-            onPressed: _incrementCounter,
-            child: Icon(Icons.add),
-          ),
-          SizedBox(
-            width: 16.0,
-          ),
-          FloatingActionButton(
-            tooltip: "Decrement Counter",
-            onPressed: _decrementCounter,
-            child: Icon(Icons.remove),
-          ),
-        ],
-      ),
-      appBar: AppBar(
-        title: Text(widget.title),
-        leading: Icon(Icons.arrow_back),
-        actions: <Widget>[
-          Icon(Icons.favorite),
-          SizedBox(
-            width: 10.0,
-          ),
-          Icon(Icons.access_alarm)
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("You have pushed this button $_counter times"),
-            Text(
-              "$_counter",
-              style: Theme.of(context).textTheme.display1,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My App "),
+        ),
+        body: Center(
+          child: Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.all(12),
+            margin: EdgeInsets.all(24),
+            constraints: BoxConstraints.expand(height: 300),
+            decoration: BoxDecoration(
+              color: Colors.orange,
+              border: Border.all(
+                width: 8.0,
+                color: Colors.red[200],
+              ),
+              borderRadius: BorderRadius.circular(12.0),
             ),
-          ],
+            child: Text(
+              'Hello world',
+              textAlign: TextAlign.center,
+            ),
+          ),
         ),
       ),
     );
