fc066b6f53d786b2504e8a435d0e9c9bc93ca3cc
Adeyoriju Olabode Wilson <whilson03@gmail.com>
2020-06-08 02:46:05 +0100

    added gesture detection for taps in bookmark_list_item_widget.dart

--- lib/widget/bookmark_list_item_widget.dart
+++ lib/widget/bookmark_list_item_widget.dart
@@ -9,20 +9,23 @@ class BookMarkListItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            bookmark.title,
-            style: Theme.of(context).textTheme.title,
-          ),
-          SizedBox(
-            height: 6,
-          ),
-          Text(bookmark.url, style: Theme.of(context).textTheme.subhead),
-        ],
+    return InkWell(
+      onTap: () {},
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              bookmark.title,
+              style: Theme.of(context).textTheme.title,
+            ),
+            SizedBox(
+              height: 6,
+            ),
+            Text(bookmark.url, style: Theme.of(context).textTheme.subhead),
+          ],
+        ),
       ),
     );
   }
